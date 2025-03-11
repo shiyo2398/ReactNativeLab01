b29657306d244b95b715aa554849397c94810a61
Shiyo Cheung <shiyo2398@gmail.com>
2025-03-11 21:02:05 +0800

    init commit

---
+++ HomeScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import {useNavigation} from '@react-navigation/native'
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to the Home Screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+  },
+});
+
+export default HomeScreen;
